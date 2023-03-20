052e38166effc3404e6b68b36f06674c73944b59
Emre Yağcı <emreygc7@gmail.com>
2023-03-20 17:49:19 +0300

    add: calendar constant

---
+++ src/utils/constant/calendar.ts
@@ -0,0 +1,17 @@
+export default {
+  months: [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ],
+  days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+}
